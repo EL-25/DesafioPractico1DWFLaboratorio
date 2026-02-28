6dbf68dea2daf2a666bce9e907fe4ce1439bcff6
Edwin Leiva <203675451+EL-25@users.noreply.github.com>
2026-02-28 13:23:12 -0600

    Subiendo proyecto

---
+++ desafio-practico/src/test/java/com/desafio/pruebas/DesafioPractico1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.pruebas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioPractico1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
